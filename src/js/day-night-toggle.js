2a42f43cedbab527797450017ef0e5317d12082a
Gaspacchio <gaspard.bariller@tutanota.com>
2020-06-08 13:53:23 +0200

    feat: add system color scheme support

--- src/js/day-night-toggle.js
+++ src/js/day-night-toggle.js
@@ -1,4 +1,23 @@
-document.querySelector(".color-toggle").addEventListener("click", (event) => {
-  document.querySelector(".color-toggle").classList.toggle("night");
+const colorToggle = document.querySelector(".color-toggle");
+
+colorToggle.addEventListener("click", (event) => {
+  colorToggle.classList.toggle("night");
   document.querySelector("body").classList.toggle("night");
 });
+
+matchMedia("(prefers-color-scheme: dark)").addListener((event) => {
+  console.log("Theme change detected");
+  if (event.matches && !colorToggle.classList.contains("night")) {
+    console.log("Auto dark theme on!");
+    colorToggle.click();
+  } else if (!event.matches && colorToggle.classList.contains("night")) {
+    console.log("Auto light theme on!");
+    colorToggle.click();
+  } else {
+    console.log("Theme is already manually set.");
+  }
+});
+
+if (matchMedia("(prefers-color-scheme: dark)").matches) {
+  colorToggle.click();
+}
